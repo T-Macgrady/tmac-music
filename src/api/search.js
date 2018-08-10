24e3916324a9798d3d4c79abd6a328d6ebd2257a
Lizhhmac <15156692746@163.com>
2018-08-10 16:40:45 +0800

    api fixed

--- src/api/search.js
+++ src/api/search.js
@@ -1,4 +1,4 @@
-import jsonp from 'common/js/jsonp'
+import jsonp from 'common/js/jsonp' 
 import { commonParams, options } from './config'
 export function getHotKey() {
   const url = 'https://c.y.qq.com/splcloud/fcgi-bin/gethotkey.fcg'
@@ -12,7 +12,7 @@ export function getHotKey() {
 }
 
 export function search(query, page, zhida, perpage) {
-  const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+  const url = '/soso/fcgi-bin/search_for_qq_cp'
 
   const data = Object.assign({}, commonParams, {
     w: query,
