92573f69954af0181aa3637ce71a80472cc37330
Lizhhmac <15156692746@163.com>
2018-08-24 21:51:56 +0800

    docs(conf): build conf

--- config/index.js
+++ config/index.js
@@ -8,8 +8,8 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        // assetsPublicPath: '//47.106.183.192:8888/tmac-music/dist/',
-        assetsPublicPath: '//localhost:8888/',
+        assetsPublicPath: '//47.106.183.192:8888/tmac-music/dist/',
+        // assetsPublicPath: '//localhost:8888/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
